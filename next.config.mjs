e23b6a12023a0de14213706554f36c0f13a0426e
ulysse mercadal <ulysse.mercadal@epitech.eu>
2025-11-26 15:23:12 +0100

    fix: deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "r-type";
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  devIndicators: false
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath: isProd ? `/${repoName}` : "",
+  assetPrefix: isProd ? `/${repoName}` : "",
 };
 
 export default withMDX(config);
